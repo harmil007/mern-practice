b58424bc3b69569850893bcc67025b45f89c96b2
Harmil Goti <harrygoti0@gmail.com>
2026-06-03 09:59:53 +0530

    fix: correct missing leading slashes in user route paths

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -28,11 +28,11 @@ router.route("/login").post(loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("refresh-token").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 router.route("/reset-password").post(verifyJWT, changeCurrentPassword);
-router.route("current-user").get(verifyJWT, getCurrentUser);
-router.route("update-account").patch(verifyJWT, updateAccountDetails);
-router.route("update-avatar").patch(verifyJWT, updateUserAvatar);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT, updateUserAvatar);
 
 router
   .route("/avatar")
